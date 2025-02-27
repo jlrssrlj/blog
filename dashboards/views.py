11a756a0c591d8abe86b40de1b99d5819c4e2aad
Jorge Ramos <ramos014@hotmail.com>
2025-02-26 20:27:19 -0500

    mantenimiento de categorias

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
+from blogs.models import Categorias
 from dashboards.forms import CategoryForm
 
 # Create your views here.
@@ -18,4 +19,20 @@ def add_category(request):
     return render(request, 'dashboard/add_category.html',context)
 
 def edit_category(request, pk):
-    return render(request, 'dashboard/edit_Category.html')
+    category = get_object_or_404(Categorias, pk=pk)
+    if request.method=='POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+    form = CategoryForm(instance=category)
+    context = {
+        'form': form,
+        'category': category,
+    }
+    return render(request, 'dashboard/edit_Category.html', context)
+
+def delete_category(request,pk):
+    category = get_object_or_404(Categorias, pk=pk)
+    category.delete()
+    return redirect('categories')
